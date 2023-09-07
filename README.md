030973abfa21caf3d34bc7c44517d6f0d9aecea5
kingcreek <israel_launion@hotmail.com>
2023-09-07 11:32:39 +0200

    I need test it

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # 42-Malloc_tester
  
+Compile in Linux:
+`gcc -shared -fPIC -o malloc_tester.so malloc_tester.c`
+
+Load shared lib:
+`LD_PRELOAD=./malloc_tester.so ./Your_Program`
+
+
+
+Compile in MacOs:
+`gcc -shared -o malloc_tester.dylib malloc_tester.c`
+
+Load shared DyLib:
+`DYLD_INSERT_LIBRARIES=./malloc_tester.dylib ./Your_Program`
---
+++ malloc_tester.c
@@ -0,0 +1,40 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdbool.h>
+
+static void* (*real_malloc)(size_t size);
+static void  (*real_free)(void *ptr);
+
+static bool simulate_malloc_failure = false;
+
+__attribute__((constructor))
+static void init()
+{
+    real_malloc = dlsym(RTLD_NEXT, "malloc");
+    real_free   = dlsym(RTLD_NEXT, "free");
+    fprintf(stderr, "init\n");
+}
+
+void *malloc(size_t size)
+{
+    if (simulate_malloc_failure) {
+        fprintf(stderr, "Simulate malloc failure? (y/n): ");
+        char response;
+        scanf(" %c", &response);
+        if (response == 'y' || response == 'Y') {
+            fprintf(stderr, "Simulated malloc failure\n");
+            return NULL;
+        }
+    }
+
+    void *ptr = real_malloc(size);
+    //fprintf(stderr, "malloc(%zd) = %p\n", size, ptr);
+    return ptr;
+}
+
+void free(void *ptr)
+{
+    real_free(ptr);
+    //fprintf(stderr, "free(%p)\n", ptr);
+}
