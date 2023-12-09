b05dd22dc588598a84e3026c0b9021090a81636a
kingcreek <israel_launion@hotmail.com>
2023-12-09 13:45:10 +0100

    Add files via upload

--- src/malloc_tester.c
+++ src/malloc_tester.c
@@ -6,7 +6,7 @@
 /*   By: imurugar <imurugar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 10:12:07 by imurugar          #+#    #+#             */
-/*   Updated: 2023/09/17 02:40:54 by imurugar         ###   ########.fr       */
+/*   Updated: 2023/12/09 13:44:29 by imurugar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,25 @@ void *callstack[MAX_CALLSTACK_SIZE];
 
 char file_path[256] = {0};
 
+#define MAX_ALLOCATIONS 99999
+
+typedef struct
+{
+	void *ptr;
+	size_t size;
+} AllocationRecord;
+
+AllocationRecord allocations[MAX_ALLOCATIONS];
+
 int ignore_malloc = 1;
-size_t allocated_bytes;
-size_t freed_bytes;
 int malloc_counter;
 int end_program = 1;
 char program_name[512] = {0};
 int totalMallocs = 0;
 
 pthread_mutex_t malloc_mutex;
-static void lock_mutex_malloc() { ignore_malloc = 1; } //Personality disorder
-static void unlock_mutex_malloc() { ignore_malloc = 0; } //His brother
+static void lock_mutex_malloc() { ignore_malloc = 1; }	 // Personality disorder
+static void unlock_mutex_malloc() { ignore_malloc = 0; } // His brother
 
 void segfault_handler(int sig) // I catch you!
 {
@@ -52,10 +60,17 @@ void close_handler(int signo)
 
 void program_finish()
 {
-	//fprintf(stdout, " tester atexit\n");
+	// fprintf(stdout, " tester atexit\n");
 	lock_mutex_malloc();
 	if (end_program == 1)
+	{
+		size_t totalMem = 0;
+		for(int i = 0; i < malloc_counter; i++)
+			totalMem += allocations[i].size;
+		if (totalMem > 0)
+			fprintf(stdout, " \n ===LEAKS=== \nMemory not released by you: %lu\n", totalMem);
 		fprintf(stdout, "Finished tester atexit\n");
+	}
 	unlock_mutex_malloc();
 }
 
@@ -73,8 +88,6 @@ __attribute__((constructor)) static void init()
 	if (home_dir != NULL)
 		totalMallocs = read_int_from_file(file_path);
 	ignore_malloc = 0;
-	allocated_bytes = 0;
-	freed_bytes = 0;
 	malloc_counter = 0;
 	get_program_name(program_name, sizeof(program_name));
 	unlock_mutex_malloc();
@@ -92,7 +105,7 @@ INTERPOSE_C_VOID(exit, (int status), (status)) //
 }
 */
 
-INTERPOSE_C(void *, malloc, (size_t sz), (sz)) //Magic
+INTERPOSE_C(void *, malloc, (size_t sz), (sz)) // Magic
 {
 	if (ignore_malloc == 0)
 	{
@@ -107,6 +120,7 @@ INTERPOSE_C(void *, malloc, (size_t sz), (sz)) //Magic
 			void *result = Real__malloc(sz);
 			return result;
 		}
+
 		caller = callstack[1];
 
 		if (caller < (void *)0x7f0000000000)
@@ -135,18 +149,17 @@ INTERPOSE_C(void *, malloc, (size_t sz), (sz)) //Magic
 					unlock_mutex_malloc();
 					return NULL;
 				}
-				// malloc tested, only count
-				malloc_counter++;
-				//}
-				// malloc already tested, only retunr real malloc
+				
 				void *result = Real__malloc(sz);
-				// allocated_bytes += malloc_usable_size(result);
+				allocations[malloc_counter].ptr = result;
+				allocations[malloc_counter].size = sz;
 				free(strings);
 				pthread_mutex_unlock(&malloc_mutex);
+				malloc_counter++;
 				unlock_mutex_malloc();
 				return result;
 			}
-			//This call does not come from the program, run!
+			// This call does not come from the program, run!
 			pthread_mutex_unlock(&malloc_mutex);
 			unlock_mutex_malloc();
 			free(strings);
@@ -154,40 +167,66 @@ INTERPOSE_C(void *, malloc, (size_t sz), (sz)) //Magic
 		}
 		unlock_mutex_malloc();
 	}
-	//I have no fucking idea where this call is coming from, but I really don't care. System defeated!
+	// I have no fucking idea where this call is coming from, but I really don't care. System defeated!
 	return Real__malloc(sz);
 }
 
-INTERPOSE_C_VOID(free, (void *p), (p)) //Maybe one day i will remember you and implement you, but for now ours cannot be
+INTERPOSE_C_VOID(free, (void *p), (p)) // Maybe one day i will remember you and implement you, but for now ours cannot be
 {
-	/*
+
 	if (ignore_malloc == 0)
 	{
-		// lock_mutex_malloc();
+		lock_mutex_malloc();
 		void *caller = NULL;
 		int size = backtrace(callstack, sizeof(callstack) / sizeof(callstack[0]));
 		char **strings = backtrace_symbols(callstack, size);
 		if (size <= 0)
 		{
-			unlock_mutex_malloc();
 			// freed_bytes += malloc_usable_size(p);
 			Real__free(p);
 			Real__free(strings);
 			unlock_mutex_malloc();
 			return;
 		}
-		caller = callstack[3];
+
+		/*
+		for(int i = 0; i < size; i++)
+		{
+			fprintf(stderr, "in free: %d %s\n", i, strings[i]);
+		}
+		*/
+
+		if (strstr(strings[1], program_name) != NULL)
+		{
+			for(int i = 0; i < malloc_counter; i++)
+			{
+				if (allocations[i].ptr == p)
+				{
+					allocations[i].size = 0;
+					break;
+				}
+			}
+			//freed_bytes += malloc_size(p);
+			//fprintf(stderr, "total freed: %lu\n", freed_bytes);
+		}
+		/*
+		caller = callstack[1];
 		if (caller < (void *)0x7f0000000000)
 		{
 			if (strstr(strings[1], program_name) != NULL)
 				freed_bytes += malloc_usable_size(p);
 			// fprintf(stderr, "freed_bytes: %zu\n", freed_bytes);
 		}
+		*/
+
 		Real__free(strings);
+		Real__free(p);
+		unlock_mutex_malloc();
+		return;
 	}
 
 	// freed_bytes += malloc_usable_size(p);
 	// unlock_mutex_malloc();
-	*/
+
 	Real__free(p);
 }
