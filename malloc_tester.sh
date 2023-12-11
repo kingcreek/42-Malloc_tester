798feb7edef52e6521e730bbc87ee7c27d0dc881
kingcreek <israel_launion@hotmail.com>
2023-12-11 20:56:23 +0100

    Update malloc_tester.sh

--- malloc_tester.sh
+++ malloc_tester.sh
@@ -20,7 +20,7 @@ FOLDER=".malloc_tester"
 ##############################################################################################
 
 ##############################################################################################
-CURRENTVERSION="1.7"
+CURRENTVERSION="1.8"
 
 github_url="https://github.com/kingcreek/42-Malloc_tester/raw/main/version.txt"
 if ! curl -s -L "$github_url" | grep -q $CURRENTVERSION; then
