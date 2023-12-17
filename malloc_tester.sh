239b09286fa187a457dd0c07a7063a773d17447a
kingcreek <israel_launion@hotmail.com>
2023-12-17 15:49:33 +0100

    Update malloc_tester.sh

--- malloc_tester.sh
+++ malloc_tester.sh
@@ -22,7 +22,7 @@ FOLDER=".malloc_tester"
 ##############################################################################################
 
 ##############################################################################################
-CURRENTVERSION="2.9.6"
+CURRENTVERSION="2.9.7"
 
 # github_url="https://github.com/kingcreek/42-Malloc_tester/raw/main/version.txt"
 # if ! curl -s -L "$github_url" | grep -q $CURRENTVERSION; then
@@ -214,13 +214,13 @@ fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	if ! readelf -S "$EXECUTABLE_PATH" | grep -q '\.debug'; then
-    	echo -e "\n\x1B[31m Your program is not compiled with -g, please compile with said flag for better results. \x1B[0m"
-		exit
+    	#	echo -e "\n\x1B[31m Your program is not compiled with -g, please compile with said flag for better results. \x1B[0m"
+	#	exit
 	fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	if ! nm -a "$EXECUTABLE_PATH" | grep 'ENSYM'; then
-    	echo -e "\n\x1B[31m Your program is not compiled with -g, please compile with said flag for better results. \x1B[0m"
-		exit
+    	#	echo -e "\n\x1B[31m Your program is not compiled with -g, please compile with said flag for better results. \x1B[0m"
+	#	exit
 	fi
 fi
 
